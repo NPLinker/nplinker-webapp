521daa9d5bb3e68ee5b26eea5dbfe8b63fccf26f
gcroci2 <crocioni.giulia@gmail.com>
2024-07-08 11:41:57 +0200

    add light/dark switch and set the same colour as mkdocs

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 import dash_bootstrap_components as dbc
 from dash import Dash
+from dash import Input
+from dash import Output
+from dash import clientside_callback
 from dash import html
 
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
-app = Dash(__name__, external_stylesheets=[dbc.themes.UNITED, dbc_css])
+app = Dash(__name__, external_stylesheets=[dbc.themes.UNITED, dbc_css, dbc.icons.FONT_AWESOME])
 
 # Define the navigation bar
 navbar = dbc.Row(
@@ -24,14 +27,37 @@ navbar = dbc.Row(
             dark=True,
         ),
     ),
-    className="mb-5",
+    className="mb-2",
+)
+
+color_mode_switch = html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="color-mode-switch"),
+        dbc.Switch(
+            id="color-mode-switch", value=False, className="d-inline-block ms-1", persistence=True
+        ),
+        dbc.Label(className="fa fa-sun", html_for="color-mode-switch"),
+    ],
+    className="p-4 position-absolute end-0 mt-2",
 )
 
 # Define the content area with tabs
-gm_content = html.Div("Tab 1 Content", className="p-4 border")
-mg_content = html.Div("Tab 2 Content", className="p-4 border")
+gm_content = dbc.Row(
+    dbc.Col(
+        dbc.Card(
+            dbc.CardBody([html.Div("Tab 1 Content", className="p-4 card-text")]),
+        )
+    )
+)
 
-# TODO: Improve the tabs stile (dbc or dcc?)
+mg_content = dbc.Row(
+    dbc.Col(
+        dbc.Card(
+            dbc.CardBody([html.Div("Tab 2 Content", className="p-4 card-text")]),
+        )
+    )
+)
+
 tabs = dbc.Row(
     dbc.Col(
         dbc.Tabs(
@@ -39,21 +65,32 @@ tabs = dbc.Row(
                 dbc.Tab(
                     gm_content,
                     label="Genomics -> Metabolomics",
-                    # selected_style={"font-size": "20px", "background-color": "#f8f9fa"},
+                    activeTabClassName="fw-bold",
                 ),
                 dbc.Tab(
                     mg_content,
                     label="Metabolomics -> Genomics",
-                    # selected_style={"font-size": "20px"},
+                    activeTabClassName="fw-bold",
                 ),
-            ]
-        )
+            ],
+        ),
     ),
     className="p-4",
 )
 
+app.layout = dbc.Container([navbar, color_mode_switch, tabs], fluid=True, className="p-0")
 
-app.layout = dbc.Container([navbar, tabs], fluid=True, className="dbc p-0")
+clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute('data-bs-theme', switchOn ? 'light' : 'dark');
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("color-mode-switch", "id"),
+    Input("color-mode-switch", "value"),
+)
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
