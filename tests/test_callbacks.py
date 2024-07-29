b9c851fd4081d117a36df0326278887d71898285
gcroci2 <crocioni.giulia@gmail.com>
2024-07-29 11:29:06 +0200

    add data dir for tests

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -1,10 +1,12 @@
 import os
+from pathlib import Path
 from dash_uploader import UploadStatus
 from app.app import disable_tabs
 from app.app import upload_data
+from . import DATA_DIR
 
 
-MOCK_FILE_PATH = os.path.join(os.path.dirname(__file__), "data/mock_obj_data.pkl")
+MOCK_FILE_PATH = Path(DATA_DIR, "mock_obj_data.pkl")
 
 
 def test_upload_data():
@@ -15,11 +17,8 @@ def test_upload_data():
     upload_string, path_string = upload_data(status)
 
     # Check the result
-    assert (
-        upload_string
-        == f"Successfully uploaded file `{os.path.basename(MOCK_FILE_PATH)}` of size 5.39 MB."
-    )
-    assert path_string == MOCK_FILE_PATH
+    assert upload_string == f"Successfully uploaded: {os.path.basename(MOCK_FILE_PATH)} [5.39 MB]"
+    assert path_string == str(MOCK_FILE_PATH)
 
 
 def test_disable_tabs():
