0644ed160478c85511c98091b6f2cf63a3928684
gcroci2 <crocioni.giulia@gmail.com>
2024-08-13 09:47:51 +0200

    remove sys.path.append

--- app/app.py
+++ app/app.py
@@ -1,10 +1,5 @@
-import os
-import sys
 from callbacks import app
 from layouts import create_layout
-
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 app.layout = create_layout()
