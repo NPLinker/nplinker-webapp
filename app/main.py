dc6c5bafadc6646595c5dca3c4168f224940f25d
gcroci2 <crocioni.giulia@gmail.com>
2025-03-24 16:24:56 +0100

    debug true

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ from layouts import create_layout
 app.layout = create_layout()
 
 if __name__ == "__main__":
-    app.run_server(debug=False, host="0.0.0.0")
+    app.run_server(debug=True, host="0.0.0.0")
